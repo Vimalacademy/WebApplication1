803a920849da3e7671ebff540ccb3abe588de0e7
CNN-COMPUTERS <CNN-COMPUTERS@DESKTOP-O53GGJV>
2023-09-15 08:35:32 +0530

    Add project files.

---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿const animatedElement = document.getElementById("animated-element");
+
+function animateElement() {
+    animatedElement.style.transform = "translateX(200px)";
+}
+
+// Add a click event listener to trigger the animation
+animatedElement.addEventListener("click", animateElement);
